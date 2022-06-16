eacec2e18e7f9785bf9553c1d5ae539960dbb6c6
blazingstark07 <pranjal3457.py@gmail.com>
2022-06-16 16:21:12 +0530

    smol error fixed

--- Challenge3/utility.js
+++ Challenge3/utility.js
@@ -1,5 +1,7 @@
 export const playAudio = (event) => {
     const getId = event.target.id;
-    var audio = new Audio(`./audio/key-${getId}.mp3`);
-    audio.play();
+    if (getId) {
+        var audio = new Audio(`./audio/key-${getId}.mp3`);
+        audio.play();
+    }
 };
