eab0b167137a8db852484e6182e26b67e842a61b
blazingstark07 <pranjal3457.py@gmail.com>
2022-06-17 13:18:50 +0530

    more readable

--- Challenge4/utility.js
+++ Challenge4/utility.js
@@ -1,19 +1,23 @@
 const getRandomJiggle = () => {
     return Math.floor(Math.random() * 53);
 };
+
 const utilityKeys = ["CAPSLOCK", "TAB", "SHIFT", "ENTER"];
 
 const startRandomJiggle = () => {
     let randomKey = getRandomJiggle();
     let randomJiggleKey = document.querySelectorAll(".key")[randomKey];
+
     randomJiggleKey.className === "key" ?
         (randomJiggleKey.className = "key jiggle") :
         (randomJiggleKey.className = "key utility jiggle");
 };
+
 const changeJiggle = (event) => {
     let jigglingKey = document.getElementsByClassName("jiggle")[0];
     let pressedKey = event.key.toUpperCase();
     let jigglingKeyName = jigglingKey.getAttribute("data-key");
+
     if (jigglingKeyName === pressedKey) {
         startRandomJiggle();
         utilityKeys.includes(jigglingKeyName) ?
@@ -21,7 +25,9 @@ const changeJiggle = (event) => {
             (jigglingKey.className = "key");
     }
 };
+
 document.addEventListener("keydown", changeJiggle);
+
 export const startJiggle = () => {
     startRandomJiggle();
 };
